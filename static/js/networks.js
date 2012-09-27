c5f95f7a033e7d54fe1e3ab41714ac4629cebf3b
Ola Sitarska <ola@sitarska.com>
2012-09-27 16:02:06 +0200

    - Added Facebook page - Removed Google+ - Bigger twitter bubble - Page unsticked from the left side

--- static/js/networks.js
+++ static/js/networks.js
@@ -1,4 +1,4 @@
-var fb_url = 'https://graph.facebook.com/112412848774431';
+var fb_url = 'https://graph.facebook.com/469934899704539';
 var tw_url = 'https://api.twitter.com/1/users/show.json?screen_name=djangocon&callback=?';
 var tw_tl_url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=djangocon&count=20&include_rts=1&callback=?';
 
@@ -15,7 +15,7 @@ function set_twitter() {
 
     $.getJSON(tw_tl_url, function(data){
         var tweet;
-        for(i=0; i < data.length; i++) {
+        for(i=0; i < 3; i++) {
             tweet = '<a target="_blank" href="https://twitter.com/djangocon/status/' +
                     data[i].id_str + '"><div class="tweet">' + 
                     data[i].text + '</div></a>';
