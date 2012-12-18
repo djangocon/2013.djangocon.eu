bc4f970522403aaf4e1a8bfb36fb2cd54fdfedd9
Ola Sitarska <ola@sitarska.com>
2012-12-18 19:07:29 +0100

    Fixed ugly hack with nice background-attachment. Thanks @patrys

--- static/js/background_scroll.js
+++ static/js/background_scroll.js
@@ -1,13 +1,14 @@
 function start_backgroundscroll(){
    $(window).scroll(function () {
         if ($(window).scrollTop() > 650){
-            $('body').css('background-position', 'center '+($(window).scrollTop()-650).toString()+"px")
+        	$('body').css('background-position', 'center -650px');
+            $('body').css('background-attachment', 'fixed');
         } else {
             $('body').css('background-position', 'center 0px');
+            $('body').css('background-attachment', 'scroll');
         }
     });
 }
-
 $(document).ready(function(){
     start_backgroundscroll();
 });
