8197660d3f17e101a62ec0822814fea4ea543b9d
Ola Sitarska <ola@sitarska.com>
2013-02-03 19:06:39 +0100

    New dynamic index/speakers page

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -8,16 +8,14 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^blog/$', TemplateView.as_view(template_name='blog.html')),
     url(r'^team/$', TemplateView.as_view(template_name='team.html')),
     url(r'^codeofconduct/$', TemplateView.as_view(template_name='codeofconduct.html')),
-    url(r'^speakers/$', TemplateView.as_view(template_name='speakers.html')),
 
-    #url(r'^vote/', include('vote.urls', namespace='vote', app_name='vote')),
-
+    url(r'', include('speakers.urls', namespace='speakers', app_name='speakers')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
+
 )
 
 if not settings.DEBUG:
