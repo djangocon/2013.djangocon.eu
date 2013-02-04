8a51d3389b8d9987d1f464eff014bb9f9e31a9a9
Ola Sitarska <ola@sitarska.com>
2013-02-04 12:18:22 +0100

    Actions to quickly hide/publish speakers/talks

--- speakers/admin.py
+++ speakers/admin.py
@@ -5,10 +5,28 @@ from .models import Speaker, Talk
 
 class SpeakerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name','title','photo','is_public']
+    actions = ('make_public', 'hide')
+
+    def make_public(self, request, queryset):
+        queryset.update(is_public=True)
+    make_public.short_description = u'Make speakers public'
+
+    def hide(self, request, queryset):
+        queryset.update(is_public=False)
+    hide.short_description = u'Hide speakers'
 
 class TalkAdmin(admin.ModelAdmin):
     list_display = ['speakers_names', 'title', 'description', 'is_public']
     list_editable = ['title','description']
+    actions = ('make_public', 'hide')
+
+    def make_public(self, request, queryset):
+        queryset.update(is_public=True)
+    make_public.short_description = u'Make talks public'
+
+    def hide(self, request, queryset):
+        queryset.update(is_public=False)
+    hide.short_description = u'Hide talks'
 
 admin.site.register(Speaker, SpeakerAdmin)
 admin.site.register(Talk, TalkAdmin)
