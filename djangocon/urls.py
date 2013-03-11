f70b6147b5459010c52518a4e91c9be47231e21e
Ola Sitarska <ola@sitarska.com>
2013-03-11 19:17:25 +0100

    Get around information

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^team/$', TemplateView.as_view(template_name='team.html')),
     url(r'^codeofconduct/$', TemplateView.as_view(template_name='codeofconduct.html')),
     url(r'^venue/$', TemplateView.as_view(template_name='venue.html')),
-    url(r'^schedule/$', TemplateView.as_view(template_name='schedule.html')),
+    url(r'^getaround/$', TemplateView.as_view(template_name='getaround.html')),
 
     url(r'', include('speakers.urls', namespace='speakers', app_name='speakers')),
     url(r'^admin/', include(admin.site.urls)),
